3f102d1260e210f9e197c3dc04dafb133218f239
felpsM67 <felipedasilvamattos50@gmail.com>
2026-01-28 02:26:28 -0300

    Atualização multer, rotas e post-document

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
+    fileSize: 50 * 1024 * 1024, // 5MB
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
